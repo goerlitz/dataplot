25ef159adf286ec6a4150368479337fea0ad466f
Olaf Görlitz <olaf.goerlitz@gmail.com>
2021-04-26 21:07:31 +0200

    first project setup

--- dataplot/dataplot.py
+++ dataplot/dataplot.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 thousand_format = FuncFormatter(lambda x, p: format(int(x), ','))
 
-def plot_cat(df: pd.DataFrame, col: str, title: str = None, figsize = None):
+
+def plot_cat(df: pd.DataFrame, col: str, title: str = None, figsize=None):
     fig, ax = plt.subplots(figsize=figsize)
     
     df[col].value_counts().plot.barh(title=title, zorder=2, ax=ax)
@@ -17,7 +18,8 @@ def plot_cat(df: pd.DataFrame, col: str, title: str = None, figsize = None):
     
     return fig
 
-def plot_cat2(df: pd.DataFrame, cols: list, title: str = None, figsize = None) -> plt.Figure:
+
+def plot_cat2(df: pd.DataFrame, cols: list, title: str = None, figsize=None) -> Figure:
     """Plot categories with subsets."""
     fig, ax = plt.subplots(figsize=figsize)
     
@@ -27,8 +29,9 @@ def plot_cat2(df: pd.DataFrame, cols: list, title: str = None, figsize = None) -
     # sort rows and columns by sum (default is lexicographic)
     row_sorted = mat_df.sum(axis=1).sort_values(ascending=True).index
     col_sorted = mat_df.sum(axis=0).sort_values(ascending=True).index
-    
     mat_df = mat_df.reindex(labels=row_sorted, columns=col_sorted)
+
+    # create plot
     mat_df.plot.barh(stacked=True, title=title, zorder=2, ax=ax)
     
     # add grid lines

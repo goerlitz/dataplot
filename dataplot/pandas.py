874612e835399892db8cdea297bc057b2245e5d8
Olaf Görlitz <olaf.goerlitz@gmail.com>
2021-04-28 16:36:03 +0200

    reworked and improved count plot

--- dataplot/pandas.py
+++ dataplot/pandas.py
@@ -6,44 +6,37 @@ import pandas as pd
 thousand_format = FuncFormatter(lambda x, p: format(int(x), ','))
 
 
-def plot_cat(df: pd.DataFrame, col: str, title: str = None, figsize=None):
-    fig, ax = plt.subplots(figsize=figsize)
-    
-    df[col].value_counts().plot.barh(title=title, zorder=2, ax=ax)
-    
-    # add grid lines
-    ax.xaxis.grid(True, linestyle='--')
-    
-    fig.tight_layout()
-    
-    return fig
+# countplot - count categories like sns.countplot (optionally by second dimension)
+# requires long table format
 
+def countplot(df: pd.DataFrame, col: str, by: str = None, title: str = None, stacked=True, annotate=None, figsize=None,
+              ax=None) -> Figure:
+    """Plot counts of categorial variables in given DataFrame."""
 
-def plot_cat2(df: pd.DataFrame, cols: list, title: str = None, stacked=True, annotate=None, figsize=None, ax=None):
-    """Plot categories with subsets."""
-
-    # todo: check that we only get two columns
-
+    # create new figure or use given Axis object for drawing
     if ax is None:
         fig, ax = plt.subplots(figsize=figsize)
     else:
         fig = None
 
-    barwidth = 0.45 if stacked else 0.85
+    # define width of bars
+    barwidth = 0.45 if (stacked or by is None) else 0.85
 
-    # get number of value combinations in columns and transform to matrix
-    mat_df = df.groupby(cols).size().unstack(level=-1, fill_value=0)
+    if by is not None:
+        # get number of value combinations in columns and transform to matrix
+        mat_df = df.groupby([col, by]).size().unstack(level=-1, fill_value=0)
 
-    # sort rows and columns by sum (default is lexicographic)
-    row_sorted = mat_df.sum(axis=1).sort_values(ascending=True).index
-    col_sorted = mat_df.sum(axis=0).sort_values(ascending=True).index
+        # reorder rows and columns by sum (default is lexicographic)
+        row_sorted = mat_df.sum(axis=1).sort_values(ascending=True).index
+        col_sorted = mat_df.sum(axis=0).sort_values(ascending=True).index
+        count_df = mat_df.reindex(labels=row_sorted, columns=col_sorted)
+    else:
+        count_df = df[col].value_counts(ascending=True).to_frame()
 
-    # reorder rows and columns and plot
-    mat_df = mat_df.reindex(labels=row_sorted, columns=col_sorted)
-    mat_df.plot.barh(stacked=stacked, title=title, zorder=2, width=barwidth, ax=ax)
+    count_df.plot.barh(stacked=stacked, title=title, zorder=2, width=barwidth, ax=ax)
 
     # compute max value
-    max_val = mat_df.max().max()
+    max_val = count_df.max().max()
     # compute total count
     points = [p.get_bbox().get_points() for p in ax.patches]
     count = int(sum([p[1, 0] - p[0, 0] for p in points]))
@@ -69,9 +62,13 @@ def plot_cat2(df: pd.DataFrame, cols: list, title: str = None, stacked=True, ann
 
             ax.annotate(s, (x, y), ha=halign, va="center", color=color)
 
+    if by is None:
+        ax.legend().remove()
+
     ax.set_ylabel(None)
 
     if fig is not None:
         fig.tight_layout()
 
     return fig
+
